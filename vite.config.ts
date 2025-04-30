85567423754315c240d5249df21e3528912a9cd0
wangjunjie <wangjunjie@yxuer.com>
2025-04-30 14:41:35 +0800

    build: 添加生产环境移除console，减少打包体积

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,17 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { wrapperEnv } from './src/utils/environment'
 import { visualizer } from 'rollup-plugin-visualizer'
-
+import removeConsole from 'vite-plugin-remove-console'
 // https://vite.dev/config/
 export default ({ mode }: ConfigEnv): UserConfigExport => {
   const env: any = wrapperEnv(loadEnv(mode, process.cwd()))
   console.log('当前环境变量：', env)
 
-  const plugins = [vue(), vueDevTools()]
+  const plugins = [
+    vue(),
+    vueDevTools(),
+    removeConsole(), // 生产环境移除 console
+  ]
 
   // 只在生成报告时添加 visualizer 插件
   if (process.env.ANALYZE) {
